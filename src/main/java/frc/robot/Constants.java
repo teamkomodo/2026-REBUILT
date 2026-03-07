8677b107fff2a431fe1be3bf5370c237d558fee9
boraayhan <boraayhanwebsite@gmail.com>
2026-03-07 06:58:59 -0700

    Changed intake speed from 0.6 to 0.7

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -155,8 +155,8 @@ public final class Constants {
     public static final int INTAKE_MOTOR_RIGHT_ID = 4;
     public static final int INTAKE_SMART_CURRENT_LIMIT = 70; // FIXME
 
-    public static final double INTAKE_INTAKE_SPEED = -0.6; // FIXME
-    public static final double INTAKE_FEED_SPEED = -0.6; // FIXME
+    public static final double INTAKE_INTAKE_SPEED = -0.7; // FIXME
+    public static final double INTAKE_FEED_SPEED = -0.7; // FIXME
     public static final double INTAKE_EJECT_SPEED = 0.8; // FIXME
 
     public static final double INTAKE_EJECT_TIME = 0.3; // FIXME
